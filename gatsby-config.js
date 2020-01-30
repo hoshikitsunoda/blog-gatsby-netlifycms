b0e26140455d119302327c5975d8908ca5f64bfe
seiki tsunoda <liquidohr@gmail.com>
2020-01-30 00:27:09 -0800

    path changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/content/blog`,
         name: `blog`,
       },
     },
