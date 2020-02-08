7c0598d8a69ecab2641893e5d8851e683411d050
seiki tsunoda <liquidohr@gmail.com>
2020-02-08 15:37:51 -0800

    added tags page template #1

---
+++ src/templates/tags.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+
+const Tags = ({ data }) => {
+  return (
+    <Layout>
+      <div>Tags</div>
+    </Layout>
+  )
+}
+
+export default Tags
